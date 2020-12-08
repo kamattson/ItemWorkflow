ef5f00e16bf2d0aa85457ed867281e24ac641185
Kris <kmattso@gmail.com>
2020-12-08 15:50:05 -0700

    comment

--- item-workflow.tests/ItemWorkflowTest.cs
+++ item-workflow.tests/ItemWorkflowTest.cs
@@ -1,10 +1,38 @@
 ﻿using System;
+using FluentAssertions;
+using item_workflow.Model;
+using item_workflow.Workflows;
+using Microsoft.Extensions.Logging.Abstractions;
+using NUnit.Framework;
+using WorkflowCore.Models;
+using WorkflowCore.Testing;
+
 namespace item_workflow.tests
 {
-    public class ItemWorkflowTest
+    [TestFixture]
+    public class ItemWorkflowTest : WorkflowTest<ItemWorkflow, Item>
     {
-        public ItemWorkflowTest()
+
+        [SetUp]
+        protected override void Setup()
         {
+            base.Setup();
         }
+
+        [Test]
+        public void NUnit_workflow_test_sample()
+        {
+            var workflowId = StartWorkflow(new Item() { Name = "foo",  Vendor = "skil" });
+            WaitForWorkflowToComplete(workflowId, TimeSpan.FromSeconds(30));
+
+            GetStatus(workflowId).Should().Be(WorkflowStatus.Complete);
+            UnhandledStepErrors.Count.Should().Be(0);
+            GetData(workflowId).Name.Should().Be("foo");
+            
+        }
+
+
+
+
     }
 }
--- item-workflow.tests/Workflow/MyWorkflow.cs
+++ item-workflow.tests/Workflow/MyWorkflow.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using WorkflowCore.Interface;
+
 namespace item_workflow.tests.Workflow
 {
-    public class MyWorkflow
+    public class MyWorkflow : IWorkflow<MyDataClass>
     {
-        public MyWorkflow()
+        public string Id => "MyWorkflow";
+        public int Version => 1;
+        public void Build(IWorkflowBuilder<MyDataClass> builder)
         {
+            builder
+                .StartWith<AddNumbers>()
+                    .Input(step => step.Input1, data => data.Value1)
+                    .Input(step => step.Input2, data => data.Value2)
+                    .Output(data => data.Value3, step => step.Output);
         }
     }
 }
