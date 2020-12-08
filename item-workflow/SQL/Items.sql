0f62319ddea8db4e0c3b1fe1f434643a6367a03b
Kris <kmattso@gmail.com>
2020-12-08 09:39:50 -0700

    approval status

--- item-workflow/SQL/Items.sql
+++ item-workflow/SQL/Items.sql
@@ -7,22 +7,23 @@ CREATE TABLE Item
     Name [NVARCHAR] (50) NOT NULL,
     ArticleSourceFlag [NVARCHAR] (1) NOT NULL,
     Vendor [NVARCHAR] (50) NOT NULL, 
+    ApprovalStatus [NVARCHAR] (1),
     LastUpdateDate [Datetime]
 NOT NULL
 )
 
 INSERT INTO Item
-    ([ItemId],[Name], [ArticleSourceFlag], [Vendor], [LastUpdateDate])
+    ([ItemId],[Name], [ArticleSourceFlag], [Vendor], [ApprovalStatus], [LastUpdateDate])
 VALUES
-    ( 1, N'Saw', N'A', N'Craftsman', CURRENT_TIMESTAMP),
-    ( 2, N'Nailer', N'B', N'Dewalt', CURRENT_TIMESTAMP),
-    ( 3, N'Drill', N'D', N'Milwaukee', CURRENT_TIMESTAMP),
-    ( 4, N'Tape Measure', N'A', N'Stanley', CURRENT_TIMESTAMP)
+    ( 1, N'Saw', N'A', N'Craftsman', '', CURRENT_TIMESTAMP),
+    ( 2, N'Nailer', N'B', N'Dewalt','', CURRENT_TIMESTAMP),
+    ( 3, N'Drill', N'D', N'Milwaukee', '',CURRENT_TIMESTAMP),
+    ( 4, N'Tape Measure', N'A', N'Stanley','', CURRENT_TIMESTAMP)
 GO
 
 SELECT COUNT(*) as ItemCount
 FROM dbo.Item;
 
-SELECT i.ItemId, i.Name, i.ArticleSourceFlag, i.Vendor, i.LastUpdateDate
+SELECT i.ItemId, i.Name, i.ArticleSourceFlag, i.Vendor, i.ApprovalStatus, i.LastUpdateDate
 FROM dbo.Item as i
 GO
